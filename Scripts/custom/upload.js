2ee44aa781cee771d362939a247ba2dc027dc553
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-01-19 10:47:44 -0500

    done

--- Scripts/custom/upload.js
+++ Scripts/custom/upload.js
@@ -1,18 +1,27 @@
-﻿function UploadFile(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+﻿$(document).ready(function () {
+    function UploadFile(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#pic-preview-wrapper img').attr('src', e.target.result);
-        };
-        $("#select-photo").text(input.files.length + " selected");
-        $("#select-photo").css({ "opacity": 0.6, "background": "#e7d7d7", "border": "5px outset #efefef00"});
+            reader.onload = function (e) {
+                $('#pic-preview-wrapper img').attr('src', e.target.result);
+            };
+            $("#select-photo").text(input.files.length + " selected");
+            $("#select-photo").css({ "opacity": 0.6, "background": "#e7d7d7", "border": "5px outset #efefef00" });
 
-        reader.readAsDataURL(input.files[0]);
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-}
 
-$("#browse-pic").change(function () {
-    $("#pic-form").css("background-image", "none");
-    UploadFile(this);
-});
+    $("#browse-pic").change(function () {
+        $("#pic-form").css("background-image", "none");
+        UploadFile(this);
+    });
+    
+    $("#submit-btn").click(function () {
+        $("#real-submit").click();
+    });
+
+
+})
+
