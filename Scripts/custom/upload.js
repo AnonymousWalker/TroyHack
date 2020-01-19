e935d1dbc226903ff252332fa2e25ef4f79426c7
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-01-19 08:43:20 -0500

    finish upload

--- Scripts/custom/upload.js
+++ Scripts/custom/upload.js
@@ -5,6 +5,8 @@
         reader.onload = function (e) {
             $('#pic-preview-wrapper img').attr('src', e.target.result);
         };
+        $("#select-photo").text(input.files.length + " selected");
+        $("#select-photo").css({ "opacity": 0.6, "background": "#e7d7d7", "border": "5px outset #efefef00"});
 
         reader.readAsDataURL(input.files[0]);
     }
